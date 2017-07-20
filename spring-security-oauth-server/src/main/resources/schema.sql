846b56ce4bb448d18b7f2ea75d4ad362a3b8f93a
yolacastillo <castillo.yola@gmail.com>
2017-07-20 10:42:47 -0500

    Migracion Oracle
    No funciona

--- spring-security-oauth-server/src/main/resources/schema.sql
+++ spring-security-oauth-server/src/main/resources/schema.sql
@@ -1,65 +1,77 @@
-drop table if exists oauth_client_details;
+drop table oauth_client_details;
+drop table oauth_client_token;
+drop table oauth_access_token;
+drop table oauth_refresh_token;
+drop table oauth_code;
+drop table oauth_approvals;
+drop table ClientDetails;
+
 create table oauth_client_details (
-  client_id VARCHAR(255) PRIMARY KEY,
-  resource_ids VARCHAR(255),
-  client_secret VARCHAR(255),
-  scope VARCHAR(255),
-  authorized_grant_types VARCHAR(255),
-  web_server_redirect_uri VARCHAR(255),
-  authorities VARCHAR(255),
+  client_id VARCHAR2(255) PRIMARY KEY,
+  resource_ids VARCHAR2(255),
+  client_secret VARCHAR2(255),
+  scope VARCHAR2(255),
+  authorized_grant_types VARCHAR2(255),
+  web_server_redirect_uri VARCHAR2(255),
+  authorities VARCHAR2(255),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
-  additional_information VARCHAR(4096),
-  autoapprove VARCHAR(255)
+  additional_information BLOB,
+  autoapprove VARCHAR2(255)
 );
 
-create table if not exists oauth_client_token (
-  token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255)
+create table oauth_client_token (
+  token_id VARCHAR2(255),
+  token BLOB,
+  authentication_id VARCHAR2(255) PRIMARY KEY,
+  user_name VARCHAR2(255),
+  client_id VARCHAR2(255)
 );
 
-create table if not exists oauth_access_token (
-  token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255),
-  authentication LONG VARBINARY,
-  refresh_token VARCHAR(255)
+create table oauth_access_token (
+  token_id VARCHAR2(255),
+  token BLOB, 
+  authentication_id VARCHAR2(255) PRIMARY KEY,
+  user_name VARCHAR2(255),
+  client_id VARCHAR2(255),
+  authentication BLOB, 
+  refresh_token VARCHAR2(255)
 );
+ 
 
-create table if not exists oauth_refresh_token (
-  token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication LONG VARBINARY
+create table oauth_refresh_token (
+  token_id VARCHAR2(255),
+  token BLOB,
+  authentication BLOB
 );
+ 
 
-create table if not exists oauth_code (
-  code VARCHAR(255), authentication LONG VARBINARY
+create table oauth_code (
+  code VARCHAR2(255),
+  authentication BLOB 
 );
+ 
 
-create table if not exists oauth_approvals (
-	userId VARCHAR(255),
-	clientId VARCHAR(255),
-	scope VARCHAR(255),
-	status VARCHAR(10),
-	expiresAt TIMESTAMP,
-	lastModifiedAt TIMESTAMP
+create table oauth_approvals (
+    userId VARCHAR2(255),
+    clientId VARCHAR2(255),
+    scope VARCHAR2(255),
+    status VARCHAR2(10),
+    expiresAt TIMESTAMP,
+    lastModifiedAt TIMESTAMP
 );
+ 
 
-create table if not exists ClientDetails (
-  appId VARCHAR(255) PRIMARY KEY,
-  resourceIds VARCHAR(255),
-  appSecret VARCHAR(255),
-  scope VARCHAR(255),
-  grantTypes VARCHAR(255),
-  redirectUrl VARCHAR(255),
-  authorities VARCHAR(255),
+create table ClientDetails (
+  appId VARCHAR2(255) PRIMARY KEY,
+  resourceIds VARCHAR2(255),
+  appSecret VARCHAR2(255),
+  scope VARCHAR2(255),
+  grantTypes VARCHAR2(255),
+  redirectUrl VARCHAR2(255),
+  authorities VARCHAR2(255),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
-  additionalInformation VARCHAR(4096),
-  autoApproveScopes VARCHAR(255)
+  additionalInformation BLOB,
+  autoApproveScopes VARCHAR2(255)
 );
